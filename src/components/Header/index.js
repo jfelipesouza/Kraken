7d54179e52a38da7f14edc4f9d5a1dce05537dce
Felipe Souza <j.felipe.18@gmail.com>
2021-05-22 17:10:15 -0300

    Limpando o codigo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,18 @@
 import React from 'react';
+
+export default function Header(){
+    return(
+
+        <header>
+            <div className='info'>
+                <div className='info--left'>
+                    <a style={{textDecoration:'none'}} href="/">Kraken</a>
+                </div>
+                <div className='info--rigth'>
+                    Inicio Rapido
+                    Fale conosco
+                </div>
+            </div>
+        </header>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
 
 
 export default function App(){
     return(
         <div>
-            
+            <Header/>
+            <div>
+            </div>
         </div>
     )
 }
